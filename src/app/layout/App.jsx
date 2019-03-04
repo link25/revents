3940cc1c2b3520a5aef0a0243ff18e5db90fbf6d
dev ads mania <dev@mania.tn>
2019-03-04 12:43:21 +0100

    Route

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router-dom";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import UserDetailed from "../../features/user/UserDetailed/UserDetailedPage";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import EventForm from "../../features/event/EventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        <Route path="/(.+)" render={() => (
+        <div>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Switch>
+            <Route path="/events" component={EventDashboard} />
+            <Route path="/events/:id" component={EventDetailedPage} />
+            <Route path="/people" component={PeopleDashboard} />
+            <Route path="/profile/:id" component={UserDetailed} />
+            <Route path="/settings" component={SettingsDashboard} />
+            <Route path="/createEvent" component={EventForm} />
+          </Switch>
         </Container>
+      </div>
+        )}/>
+
       </div>
     );
   }
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,27 +1,62 @@
-import React, { Component } from 'react'
-import { Menu, Container, Button } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
 
- class NavBar extends Component {
+class NavBar extends Component {
+  state = {
+    authenticated: false
+  };
+
+  handleSignedIn = () => {
+    this.setState({
+      authenticated: true
+    });
+  };
+
+  handleSignedOut = () => {
+    this.setState({
+      authenticated: false
+    });
+    this.props.history.push('/')
+  };
+
   render() {
+    const { authenticated } = this.state;
     return (
-            <Menu inverted fixed="top">
-              <Container>
-                <Menu.Item header>
-                  <img src="assets/logo.png" alt="logo" />
-                  Re-vents
-                </Menu.Item>
-                <Menu.Item name="Events" />
-                <Menu.Item>
-                  <Button floated="right" positive inverted content="Create Event" />
-                </Menu.Item>
-                <Menu.Item position="right">
-                  <Button basic inverted content="Login" />
-                  <Button basic inverted content="Sign Out" style={{marginLeft: '0.5em'}} />
-                </Menu.Item>
-              </Container>
-            </Menu>
-    )
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item as={Link} to="/" header>
+            <img src="/assets/logo.png" alt="logo" />
+            Re-vents
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          {authenticated && 
+          <Menu.Item as={NavLink} to="/people" name="People" />}
+
+          {authenticated &&
+
+          <Menu.Item>
+            <Button
+              as={Link}
+              to="/createEvent"
+              floated="right"
+              positive
+              inverted
+              content="Create Event"
+            />
+          </Menu.Item>}
+          {authenticated ? (
+            <SignedInMenu signOut={this.handleSignedOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignedIn} />
+          )}
+        </Container>
+      </Menu>
+    );
   }
 }
 
-    export default NavBar
+export default withRouter(NavBar);
+
